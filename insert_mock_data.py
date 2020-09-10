d34af9e9c38557bc506339d9f53365483eee305b
Andrei Bessmertnyi <an6194@gmail.com>
2020-09-10 17:19:15 +0300

    update initial data script

--- insert_mock_data.py
+++ insert_mock_data.py
@@ -1,27 +1,44 @@
 from datetime import datetime
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stepik_hh.settings')
+django.setup()
+
 
 import mock_data as data
 
 from app_home.models import Company
 from app_vacancy.models import Vacancy, Specialty
 
-for specialty in data.specialties:
-    Specialty.objects.create(code=specialty['code'], title=specialty['title'])
 
-for company in data.companies:
-    Company.objects.create(name=company['title'], employee_count=0)
+if __name__ == '__main__':
 
-for job in data.jobs:
-    Vacancy.objects.create(title=job['title'],
-                           specialty=Specialty.objects.get(code=job['cat']),
-                           company=Company.objects.get(name=job['company']),
-                           salary_min=job['salary_from'],
-                           salary_max=job['salary_to'],
-                           published_at=datetime.strptime(job['posted'], '%Y-%m-%d'),
-                           description=job['desc']
-                           )
+    Specialty.objects.bulk_create(
+        [
+            Specialty(code=specialty['code'], title=specialty['title'])
+            for specialty in data.specialties
+        ]
+    )
 
-# To execute from InteractiveConsole run:
-# with open("insert_mock_data.py") as f:
-#     code = compile(f.read(), "insert_mock_data.py", 'exec')
-#     exec(code)
+    Company.objects.bulk_create(
+        [
+            Company(name=company['title'], employee_count=0)
+            for company in data.companies
+        ]
+    )
+
+    Vacancy.objects.bulk_create(
+        [
+            Vacancy(
+                title=job['title'],
+                specialty=Specialty.objects.get(code=job['cat']),
+                company=Company.objects.get(name=job['company']),
+                salary_min=job['salary_from'],
+                salary_max=job['salary_to'],
+                published_at=datetime.strptime(job['posted'], '%Y-%m-%d'),
+                description=job['desc'],
+            )
+            for job in data.jobs
+        ]
+    )
